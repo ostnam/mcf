8c776e016e0e17effa16ae00edbfe3e769e391d1
ostnam <tsougaevm@gmail.com>
2020-08-02 17:27:12 +0200

    added equivalent annual cashflow/cost

--- mcf.py
+++ mcf.py
@@ -7,11 +7,19 @@ def discount_cf(cashflow, discount_rate):
             npv = npv + (current_flow / (discount_rate + 1) ** i)
     except:
         "\n Something went wrong while discounting the cash flows. Your cashflow format might be incorrect, I recommend using a list of floats."
-    return(npv)
+    return npv
 
 
 def WACC_calculator(ReturnOnDebt, ReturnOnEquity, DebtMarketValue, EquityMarketValue, CorporateTaxRate):
-    "Returns an after-tax WACC. Percents must be in the format 0.1 for 10%"
+    """Returns an after-tax WACC. Percents must be in the format 0.1 for 10%"""
     total_capital_value = EquityMarketValue + DebtMarketValue
     WACC = (ReturnOnDebt * (1 - CorporateTaxRate)*(DebtMarketValue/total_capital_value) + (ReturnOnEquity * (EquityMarketValue/total_capital_value)))
-    return WACC
+    return WACC 
+
+def equivalent_annual_cashflow(cashflow, discount_rate):
+    """Returns the EAC of a list of cashflows. Discount rate must be in the format 0.1 for 10%."""
+    NPV = discount_cf(cashflow, discount_rate)
+    project_duration = len(cashflow) - 1
+    annuity = (1-(1/((1+discount_rate) ** project_duration))) / (discount_rate)
+    EAC = NPV/annuity
+    return EAC
