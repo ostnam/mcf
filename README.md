24b26994bc08122413b765747008bf5fda056ab4
ostnam <tsougaevm@gmail.com>
2020-11-02 12:49:45 +0100

    Created the CashFlows class, switched 2 of the functions as methods of that class.

--- README.md
+++ README.md
@@ -1,22 +1,40 @@
 # mCF
-mCF is a library of common financial functions written entirely in python3.
+mCF is a library of common financial functions that I wrote in python3 for myself.
 
-## Functions
-### discount\_cf(cashflow, discount\_rate)
-Returns the NPV of the cash flows.
+## CashFlows class
+You can create a new cashflow instance with mcf.CashFlows().
 
-I recommend the use a list of floats representing the consecutive cashflows as the "cashflow" argument. 
+    In[1]: import mcf
+    In[2]: x = [10, 10]
+    In[3]: y = mcf.CashFlows(x)
+    In[4]: y.cashflows
+    Out[4]: [10, 10]
 
+I recommend using a list of integers or floats as input.
 The first cashflow is considered as occurring "right now", and thus not discounted. 
+### CashFlows methods
+#### CashFlows.discount(discount_rate)
+Returns the NPV of the cashflows at the specified discount rate. The discount rate should be in the format 0.10 for 10%.
 
+    In[5]: y.discount(0.1)
+    Out[5]: 19.09090909090909
+
+#### CashFlows.eac(discount_rate)
+Returns the equivalent annual cost of the cashflows at the specified discount rate.
+
+    In[6]: y.eac(0.1)
+    Out[6]: 20.999999999999993
+
+#### CashFlows.pv(n, discount_rate)
+Returns the present value of the n+1^th cash flow at the specified discount rate.
+
+    In[7]: y.pv(0, 0.1)
+    Out[7]: 10.0
+    In[8]: y.pv(1, 0.1)  
+    Out[8]: 9.09090909090909  
+
+## Other functions
 ### WACC_calculator(return_on_debt, return_on_equity, debt_market_value, equity_market_value, corporate_tax_rate)
 Returns an after-tax WACC.
 
 The rates should be in the 0.\* format.
-
-### equivalent\_annual\_cashflow(cashflow, discount\_rate)
-Returns the equivalent annual cashflow/cost of a list of cashflows.
-
-Just like discount\_cf(),  I recommend using a list of floats representing the consecutive cashflows as the "cashflow" argument.
-
-The first cash flow is considered as occurring "right now" and thus isn't discounted.
--- mcf.py
+++ mcf.py
@@ -1,13 +1,31 @@
-def discount_cf(cashflow, discount_rate):
-    """Calculates the NPV of a list of cashflows, with a single discount rate. It is assumed that the first cashflow occurs immediately and thus isn't discounted. The discount rate must be in the format 0.1 for 10%."""
-    npv = 0.0
-    try:
-        for i in range(0, len(cashflow)):
-            current_flow = cashflow[i]
-            npv = npv + (current_flow / (discount_rate + 1) ** i)
-    except:
-        "\n Something went wrong while discounting the cash flows. Your cashflow format might be incorrect, I recommend using a list of floats."
-    return npv
+class CashFlows:
+    def __init__(self, cashflows):
+        self.cashflows = cashflows
+    
+    def discount(self, discount_rate):
+        """Calculates the NPV of a list of cashflows, with a single discount rate. It is assumed that the first cashflow occurs immediately and thus isn't discounted. The discount rate must be in the format 0.1 for 10%."""
+        npv = 0.0
+        cashflow = self.cashflows
+        try:
+            for i in range(0, len(cashflow)):
+                current_flow = cashflow[i]
+                npv += (current_flow / ((discount_rate + 1) ** i))
+        except:
+            print("\n Something went wrong while discounting the cash flows. Your cashflow format might be incorrect, I recommend using a list of floats.")
+        return npv
+
+    def eac(self, discount_rate):
+        """Returns the EAC of the cashflows at the specified discount rate. Discount rate must be in the format 0.1 for 10%."""
+        NPV = self.discount(discount_rate)
+        project_duration = len(self.cashflows) - 1
+        annuity = (1-(1/((1+discount_rate) ** project_duration))) / (discount_rate)
+        EACvalue = NPV/annuity
+        return EACvalue
+
+    def pv(self, n, discount_rate):
+        """Returns the present value of the cashflow i"""
+        present_value = (self.cashflows[n] / ((discount_rate + 1) ** n))
+        return present_value
 
 
 def WACC_calculator(return_on_debt, return_on_equity, debt_market_value, equity_market_value, corporate_tax_rate):
@@ -16,10 +34,3 @@ def WACC_calculator(return_on_debt, return_on_equity, debt_market_value, equity_
     WACC = (return_on_debt * (1 - corporate_tax_rate)*(debt_market_value/total_capital_value) + (return_on_equity * (equity_market_value/total_capital_value)))
     return WACC 
 
-def equivalent_annual_cashflow(cashflow, discount_rate):
-    """Returns the EAC of a list of cashflows. Discount rate must be in the format 0.1 for 10%."""
-    NPV = discount_cf(cashflow, discount_rate)
-    project_duration = len(cashflow) - 1
-    annuity = (1-(1/((1+discount_rate) ** project_duration))) / (discount_rate)
-    EAC = NPV/annuity
-    return EAC
